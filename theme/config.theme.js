ae3e78b8fec72de80700be0bb2ece67f419cb6ac
Rub21 <rub2106@gmail.com>
2024-02-23 16:54:40 -0500

    Updates

--- theme/config.theme.js
+++ theme/config.theme.js
@@ -1,2 +1,2 @@
 Nominatim_Config.Page_Title = 'PaslestineOpenMaps';
-Nominatim_Config.Nominatim_API_Endpoint = 'http://pom.masrad.org:8083';
+Nominatim_Config.Nominatim_API_Endpoint = 'http://pom.masrad.org:8083/';
